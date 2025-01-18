54365e0a20dea615af20d5dda52fa013766b11d0
lnadenau <lukas.nadenau@inform-software.com>
2025-01-19 00:25:57 +0100

    removed old sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 import { Hero } from "@/components/hero"
-import { About } from "@/components/about"
-import { TargetUsers } from "@/components/target-users"
 import { Waitlist } from "@/components/waitlist"
 
 export default function Page() {
   return (
     <main>
       <Hero />
-      <About />
-      <TargetUsers />
       <Waitlist />
     </main>
   )
