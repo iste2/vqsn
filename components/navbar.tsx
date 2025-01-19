08882a00b00affd562fff84fa537497bd7ebbb16
lnadenau <lukas.nadenau@inform-software.com>
2025-01-19 01:05:11 +0100

    stick to top

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,7 +12,7 @@ export function Navbar() {
   }
 
   return (
-    <nav className="border-b bg-background">
+    <nav className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="flex h-16 items-center px-4 container mx-auto justify-between">
       <Link href="/" className="flex items-center gap-2">
           <span className="text-xl font-semibold tracking-tight">
