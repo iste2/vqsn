3f3dde4133aaa89bb6611c88cf4e6fbedd8a73a7
lnadenau <lukas.nadenau@inform-software.com>
2025-01-24 21:48:04 +0100

    firebase hosting

---
+++ firebase/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCz9PmdkL6Rty7iwLkq5tP1zOh-gLPhBek",
+  authDomain: "vqsn-eb9be.firebaseapp.com",
+  projectId: "vqsn-eb9be",
+  storageBucket: "vqsn-eb9be.firebasestorage.app",
+  messagingSenderId: "486497672261",
+  appId: "1:486497672261:web:dc1a8ee5330c3ba631172c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
