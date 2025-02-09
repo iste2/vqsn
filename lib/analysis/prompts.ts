3698a2e5ad3a43d160cecf42e662221d081fd410
lnadenau <lukas.nadenau@inform-software.com>
2025-02-09 15:18:41 +0100

    prompts

---
+++ lib/analysis/prompts.ts
@@ -0,0 +1,17 @@
+export const businessModelIntroduction = '' +
+    '# Business Model Analyst \n' +
+    'You are an experienced business analyst tasked with analyzing and categorizing business models. \n' +
+    '# Your Task \n' +
+    'Systematically analyze the described business model for the following characteristic and evaluate how strongly it is pronounced. Use a scale from 0 (does not apply at all) to 10 (applies completely).' +
+    '# Characteristic to Analyze: \n';
+
+export const shortLifeCycleBrands = 'Short Life Cycle Brands \n' +
+    '   - Products are purchased regularly/frequently\n' +
+    '   - Strong brand presence\n' +
+    '   - Short product life cycle\n' +
+    '   - Products with short resale cycles\n' +
+    '   - Products with associated maintenance/service contracts';
+    
+
+
+
